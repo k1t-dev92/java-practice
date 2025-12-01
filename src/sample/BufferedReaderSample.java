a4dd9f3690378af9d53120c3d15d051787a571b5
Keita Takahashi <takanenokei@gmail.com>
2025-12-01 10:59:45 +0900

    feat: add BufferedReaderSample and ScannerSample classes

---
+++ src/sample/BufferedReaderSample.java
@@ -0,0 +1,26 @@
+package sample;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BufferedReaderSample {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Please enter an integer >> ");
+        String i_str = reader.readLine();
+        int i_num = Integer.parseInt(i_str);
+
+        System.out.print("Please enter a real number >> ");
+        String d_str = reader.readLine();
+        double d_num = Double.parseDouble(d_str);
+
+        System.out.print("Please enter a word >> ");
+        String str = reader.readLine();
+
+        System.out.println("i_num = " + i_num);
+        System.out.println("d_num = " + d_num);
+        System.out.println("str = " + str);
+    }
+}
---
+++ src/sample/ScannerSample.java
@@ -0,0 +1,24 @@
+package sample;
+
+import java.util.Scanner;
+
+public class ScannerSample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter an integer >> ");
+        int i_num = scanner.nextInt();
+
+        System.out.print("Please enter a real number >> ");
+        double d_num = scanner.nextDouble();
+
+        System.out.print("Please enter a word >> ");
+        String str = scanner.next();
+
+        System.out.println("i_num = " + i_num);
+        System.out.println("d_num = " + d_num);
+        System.out.println("str = " + str);
+
+        scanner.close();
+    }
+}
