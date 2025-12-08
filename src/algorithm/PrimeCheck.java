c291503cb966c94f2ddb6c674962eeb2c77ea6f0
Keita Takahashi <takanenokei@gmail.com>
2025-12-08 23:00:05 +0900

    feat: add PrimeCheck class

---
+++ src/algorithm/PrimeCheck.java
@@ -0,0 +1,33 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class PrimeCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Input an integer >> ");
+        int num = scanner.nextInt();
+
+        boolean isPrime = true;
+
+        if (num == 1) {
+            isPrime = false;
+        }
+
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+
+        scanner.close();
+    }
+}
