4ac0251293efee59b03df68d62a6cfef4be72fa2
Keita Takahashi <takanenokei@gmail.com>
2025-12-17 02:15:33 +0900

    feat: add Factorial class

---
+++ src/algorithm/Factorial.java
@@ -0,0 +1,20 @@
+package algorithm;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer >> ");
+        int number = scanner.nextInt();
+        int factorial = 1;
+
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
+        }
+        System.out.println(number + "! = " + factorial);
+
+        scanner.close();
+    }
+}
